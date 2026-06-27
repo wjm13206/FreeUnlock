6297524a91770bec5df8c5087bca7e8d46c209af
wjm13206 <abc82864d@gmail.com>
2026-06-27 16:38:10 -0300

    feat(app): 升级应用版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "xyz.wjm13206.freeunlock"
         minSdk = 30
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
