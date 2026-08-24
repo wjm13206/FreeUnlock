80057b7746f702d3c6adee56bb4af582b03129fd
0988 <abc82864d@gmail.com>
2026-08-24 13:03:04 -0300

    chore(app): 更新版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "xyz.wjm13206.freeunlock"
         minSdk = 30
         targetSdk = 37
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.3.1"
     }
 
     signingConfigs {
